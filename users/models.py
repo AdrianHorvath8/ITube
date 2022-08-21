30c4397b79d4fe961650474b1e1bac8df6e56025
Adrian Horvath <adrian.horvath.ah8@gmail.com>
2022-08-21 15:05:44 +0200

    profile and video modelsň

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+from videos.models import Video
 
-# Create your models here.
+class Profile(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+    primary_key=True, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    channel_name = models.CharField(max_length=150, null=True, blank=True, unique=True)
+    profile_image = models.ImageField(null=False, blank=False, upload_to="profiles/", default="profiles/user-default.png")
+    profile_thumbnail = models.ImageField(null=False, blank=False, upload_to="profiles/", default="profiles/user-default.png")
+    private_list = models.ForeignKey(Video, on_delete=models.SET_NULL, null=True, blank=True)
+    public_list = models.ForeignKey(Video, on_delete=models.SET_NULL, null=True, blank=True)
+    email = models.EmailField(max_length=200,null=True, blank=True)
+    short_info = models.CharField(max_length=200, null=True, blank=True)
+    followers = models.ManyToManyField(User,related_name="followers", blank=True)
+    history = models.ManyToManyField(Video, blank=True)
+    favorite_video = models.ManyToManyField(Video, blank=True)
+    facebook_link = models.CharField(max_length=1000, null=True, blank=True)
+    instagram_link = models.CharField(max_length=1000, null=True, blank=True)
+    twitter_link = models.CharField(max_length=1000, null=True, blank=True)
+    spotify_link = models.CharField(max_length=1000, null=True, blank=True)
+    itunes_link = models.CharField(max_length=1000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.username)
+
--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,20 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+import uuid
+
+
+class Video(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+    primary_key=True, editable=False)
+    title = models.CharField(blank=True, null=True)
+    #body = models.FileField(upload_to=)
+    #views = models.ManyToManyField(Profile)
+    #like = models.ManyToManyField(Profile)
+    #dislike = models.ManyToManyField(Profile)
+    #comments = models.ForeignKey(Comments)
+    description = models.TextField(blank=True, null=True, max_length= 500)
+    #tag = models.ManyToManyField(Profile)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
