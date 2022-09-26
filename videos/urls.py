62689ccf8563794460301d75c68bf6f0f102c240
Adrian Horvath <adrian.horvath.ah8@gmail.com>
2022-09-26 17:31:27 +0200

    separating videos

--- videos/urls.py
+++ videos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("video/<str:pk>/", views.video, name="video")
 ]
--- videos/views.py
+++ videos/views.py
@@ -6,3 +6,10 @@ def home(request):
     viedos = Video.objects.all()
     context = {"videos":viedos}
     return render(request, "videos/home.html", context)
+
+
+def video(request, pk):
+
+    video = Video.objects.get(id=pk)
+    context = {"video":video}
+    return render(request, "videos/video.html", context)
