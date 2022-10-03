9b2e22f9a0e4cc0ae129c96baead22424c4e97c0
Adrian Horvath <adrian.horvath.ah8@gmail.com>
2022-10-03 16:54:12 +0200

    home frontend

--- videos/views.py
+++ videos/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render
 
 from .models import Video, Comment
 
+
 def home(request):
+    
+
     viedos = Video.objects.all()
-    context = {"videos":viedos}
+    
+    context = {"videos":viedos,}
     return render(request, "videos/home.html", context)
 
 
